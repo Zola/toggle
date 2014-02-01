c0d50aa1a9b5f8a230eb5a36d1ce94d1018cb68f
Hsiaoming Yang <me@lepture.com>
2014-02-02 00:49:53 +0800

    Init commits

---
+++ Readme.md
@@ -0,0 +1,22 @@
+
+# toggle
+
+  Toggle UI
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install yuehu/toggle
+
+## API
+
+```js
+toggle(el);
+toggle(el).on();
+toggle(el).off();
+```
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,64 @@
+
+var classes = require('classes');
+var events = require('event');
+
+
+function Toggle(el, placeholder) {
+  if (!(this instanceof Toggle)) {
+    return new Toggle(el, placeholder);
+  }
+  el._class = classes(el);
+
+  hover(el);
+
+  var me = this;
+  me.element = el;
+  me.placeholder = placeholder || el;
+
+  events.bind(el, 'click', function(e) {
+    e.preventDefault();
+    if (el._class.has('active')) {
+      el._class.remove('active');
+      me.off();
+    } else {
+      el._class.add('active');
+      me.on();
+    }
+  });
+}
+
+Toggle.prototype.on = function() {
+  this.placeholder.innerHTML = this.element.getAttribute('data-on');
+};
+
+Toggle.prototype.off = function() {
+  this.placeholder.innerHTML = this.element.getAttribute('data-off');
+};
+
+module.exports = Toggle;
+
+
+function hover(el) {
+  events.bind(el, 'mouseenter', function() {
+    var text;
+    if (el._class.has('active')) {
+      // on
+      text = el.getAttribute('data-on-hover');
+    } else {
+      // off
+      text = el.getAttribute('data-off-hover');
+    }
+    if (text) {
+      el.innerHTML = text;
+    }
+  });
+  events.bind(el, 'mouseleave', function() {
+    var text;
+    if (el._class.has('active')) {
+      text = el.getAttribute('data-on');
+    } else {
+      text = el.getAttribute('data-off');
+    }
+    el.innerHTML = text;
+  });
+}
