7073e357a5f22faffa9dc4ff54195323eb3386f8
Hsiaoming Yang <me@lepture.com>
2014-03-24 16:46:20 +0800

    New way for toggle

--- index.js
+++ index.js
@@ -6,30 +6,33 @@
  * Copyright (c) 2014 by Hsiaoming Yang.
  */
 
+var query = require('query');
 var classes = require('classes');
 var events = require('event');
 var emitter = require('emitter');
 
 
-function Toggle(el, placeholder) {
+function Toggle(el) {
   if (!(this instanceof Toggle)) {
-    return new Toggle(el, placeholder);
+    return new Toggle(el);
   }
-  el._class = classes(el);
+  el._class = el._class || classes(el);
+  el._class.add('toggle');
 
   var me = this;
   me.el = el;
-  me.placeholder = placeholder || el;
 
-  me.dataOn = el.getAttribute('data-on') || '';
-  me.dataOff = el.getAttribute('data-off') || '';
-  me.dataOnHover = el.getAttribute('data-on-hover') || '';
-  me.dataOffHover = el.getAttribute('data-off-hover') || '';
+  if (!query('.toggle-hover-active', el)) {
+    query('.toggle-inner-active', el).className += ' toggle-hover-active';
+  }
 
-  hover(el, me);
+  if (!query('.toggle-hover-inactive', el)) {
+    query('.toggle-inner-inactive', el).className += ' toggle-hover-inactive';
+  }
+
   events.bind(el, 'click', function(e) {
     e.preventDefault();
-    if (el._class.has('active')) {
+    if (el._class.has('toggle-active')) {
       me.emit('inactive');
       me.inactive();
     } else {
@@ -37,60 +40,17 @@ function Toggle(el, placeholder) {
       me.active();
     }
   });
-
-  setTimeout(mark(me));
 }
 emitter(Toggle.prototype);
 
 // show on label
 Toggle.prototype.active = function() {
-  this.el._class.add('active');
-  this.placeholder.innerHTML = this.dataOn;
+  this.el._class.add('toggle-active');
 };
 
 // show off label
 Toggle.prototype.inactive = function() {
-  this.el._class.remove('active');
-  this.placeholder.innerHTML = this.dataOff;
+  this.el._class.remove('toggle-active');
 };
 
 module.exports = Toggle;
-
-
-/**
- * Bind hover event for the given element.
- */
-function hover(el, ctx) {
-  events.bind(el, 'mouseenter', function() {
-    var text;
-    if (el._class.has('active')) {
-      // on
-      text = ctx.dataOnHover;
-    } else {
-      // off
-      text = ctx.dataOffHover;
-    }
-    if (text) {
-      el.innerHTML = text;
-    }
-  });
-  events.bind(el, 'mouseleave', function() {
-    var text;
-    if (el._class.has('active')) {
-      text = ctx.dataOn;
-    } else {
-      text = ctx.dataOff;
-    }
-    el.innerHTML = text;
-  });
-}
-
-function mark(t) {
-  return function() {
-    if (t.el._class.has('active')) {
-      t.active();
-    } else {
-      t.inactive();
-    }
-  }
-}
